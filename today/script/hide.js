7ab3ec5add2c6fc3dc3c30c93e89da891df7c9ff
YeetbenderDamB <53065389+YeetbenderDamB@users.noreply.github.com>
2020-05-10 20:46:36 -0700

    Added Auto Scroll and Fixed Up Styling

--- today/script/hide.js
+++
@@ -1,6 +0,0 @@
-function hide(element) {
-    $(`#${element}`).addClass('animShrinkOut');
-    setTimeout(function() {
-        $(`#${element}`).addClass('none');
-    }, 2000);
-}
--- today/script/show.js
+++ today/script/show.js
@@ -1,4 +1,20 @@
 function show(element) {
-    $(`#${element}`).removeClass('none');
+    let elem = document.getElementById(element);
+    if (element == 'phone') {
+        $(elem).removeClass('none');
+        $(`#show${element}`).addClass('none');
+        return;
+    };
+    $(elem).removeClass('none');
+    $(elem).addClass('show');
     $(`#show${element}`).addClass('none');
+    if ((element == 4) || (element == 5)) {
+        $('html,body').animate({
+            scrollTop: window.scrollY + 200
+        });
+        return;
+    }
+    document.getElementById(`${element}`).scrollIntoView({
+        behavior: 'smooth'
+    });
 }
