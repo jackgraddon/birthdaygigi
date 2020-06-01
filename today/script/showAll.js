21e3e183b5f5be62aacea722dd3e0490c980743c
jackgraddon <jjg0704@icloud.com>
2020-05-31 18:13:57 -0700

    Adding extras

--- today/script/showAll.js
+++ today/script/showAll.js
@@ -2,7 +2,7 @@ $(document).bind('keyup', function(e){
     if(e.which==220) {
       // "\"
       let i = 1;
-      let n = 11;
+      let n = 12;
       while (i <= n) {
           $(`#${i}`).removeClass('none');
           i++;
