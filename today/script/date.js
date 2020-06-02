0564f15a6055b5e5681081e7db204699adfd8778
jackgraddon <jjg0704@icloud.com>
2020-06-02 11:49:53 -0700

    Added ability to not be able to get back to surprise page after using

--- today/script/date.js
+++ today/script/date.js
@@ -1,13 +1,15 @@
 var date = new Date();
 let hour = date.getHours()
 let minute = date.getMinutes();
-let time = `${hour}:${minute}`;
+// let time = `${hour}:${minute}`;
+let time = `21:17`;
 console.log(time);
 
 let checkTime = function check() {
+    if (Cookies.get('surprise') === 1)
     if (time === '21:17') {
         window.location.href = "917/";
+        Cookies.set('surprise', 1, { expires: 1 });
     }
 }
-
 setInterval(checkTime, 5000);
